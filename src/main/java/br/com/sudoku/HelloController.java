fe5e183a3336ae88ecb1e36b2514246c4f96dcff
bruno22007 <brunoalmeida22007@gmail.com>
2026-05-10 11:34:44 -0300

    -Fix index of the buttons -Index of the Grids

--- src/main/java/br/com/sudoku/HelloController.java
+++ src/main/java/br/com/sudoku/HelloController.java
@@ -6,10 +6,12 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.GridPane;
 
 import java.security.Key;
 
 public class HelloController {
+    private SudokuGameLogic gameLogic = new SudokuGameLogic();
     @FXML
     private Label welcomeText;
     private Button selectedButton;
@@ -24,6 +26,23 @@ public class HelloController {
         selectedButton.setStyle("-fx-background-color: #b3e5fc; -fx-border-color: #03a9f4;");
         selectedButton.requestFocus(); //Request focus so the button "hears" the keyboard
 
+        Integer rowIndex = GridPane.getRowIndex(selectedButton);
+        Integer colIndex = GridPane.getColumnIndex(selectedButton);
+
+        int row = rowIndex == null ? 0 : rowIndex;
+        int col = colIndex == null ? 0 : colIndex;
+
+        GridPane parentBlock = (GridPane) selectedButton.getParent();
+        Integer bRowIndex = GridPane.getRowIndex(parentBlock);
+        Integer bColIndex = GridPane.getColumnIndex(parentBlock);
+
+        int blockRow = bRowIndex == null ? 0 : bRowIndex;
+        int blockCol = bColIndex == null ? 0 : bColIndex;
+
+        row += blockRow * 3;
+        col += blockCol * 3;
+
+        System.out.printf("%d %d%n", row, col);
     }
 
     @FXML
--- src/main/java/br/com/sudoku/SudokuGameLogic.java
+++ src/main/java/br/com/sudoku/SudokuGameLogic.java
@@ -1,4 +1,14 @@
 package br.com.sudoku;
 
 public class SudokuGameLogic {
+    private int[][] board = new int[9][9];
+
+    public boolean isValidMove (int row, int col, int value)
+    {
+        return true;
+    }
+
+    public void setCellValue(int row, int col, int value) {
+        board[row][col] = value;
+    }
 }
